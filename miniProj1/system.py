3a17e3a3c6e07ebc50706b5f13ab1ead0f62e3df
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-10-17 12:02:08 -0600

    jawad updates

--- miniProj1/system.py
+++ miniProj1/system.py
@@ -1,32 +1,51 @@
 
 
 def main():
+	# first screen
 	print("Welcome to the program.")
 	print("Enter your login details:")
-	username = input("User ID: ")
-	password = input("Password: ")
-	# Each user of the system should be able to login using a valid user id and password, 
-	# denoted with uid and pwd in table users.
-	print("Choose your operations:")
+	uid = input("User ID: ")
+	pwd = input("Password: ")
+	# compare these uid and pwd input variables with uid and pwd inside the users table.
+	# be able to get the user type (Eg. registry agent) and put that into a variable
+
+	operation_choice(user_type="registry agent")
+
+	
 
 	# The operations are different for each user type
-	registry_agent_options = [
-		"Register a birth", 
-		"Register a marriage", 
-		"Renew a vehicle registration", 
-		"Process a bill of sale", 
-		"Process a payment",
-		"Get a driver abstract",
-		]
+	
 
-	traffic_officer_options = [
-		"Issue a ticket",
-		"Find a car owner"
+	
+
+	
+
+
+def operation_choice(user_type):
+	options_list = []
+	print("Choose your operations: ")
+
+
+	if user_type == "registry agent":
+		print("blah blah")
+
+		options_list = [
+			"Register a birth", 
+			"Register a marriage", 
+			"Renew a vehicle registration", 
+			"Process a bill of sale", 
+			"Process a payment",
+			"Get a driver abstract",
+		]
+	else if user_type == "traffic officer":
+		print("poooooop")
+		options_list = [
+			"Issue a ticket",
+			"Find a car owner"
 		]
 
 	for i in range(len(options)):
 		print(options[i])
-
 
 
 def register_a_birth():
