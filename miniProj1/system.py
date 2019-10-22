ec1fc1ff9ee48342d9d023aadce6cc67f56ff7e2
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-10-22 11:30:31 -0600

    lots of changes, and nearly completed the issue_ticket function

--- miniProj1/system.py
+++ miniProj1/system.py
@@ -1,7 +1,9 @@
 import sqlite3
+import random
 
 connection = None
 cursor = None
+uniqueTNOs = []
 
 def main():
 	global connection, cursor
@@ -13,26 +15,24 @@ def main():
 	# first screen
 	print("Welcome to the program.")
 	
-
-	prompt = ''
-	
-	while prompt != 'q':
-		print()
-		print("Enter your login details:")
+	valid = False
+	while valid == False:
+		print("\nEnter your login details:")
 		uid = input("User ID: ")
 		pwd = input("Password: ")
 		print()
-		# compare these uid and pwd input variables with uid and pwd inside the users table.
-		# be able to get the user type (Eg. registry agent) and put that into a variable
 		cursor.execute("select utype from users where uid=? and pwd=?", (uid, pwd))
 		utype = cursor.fetchall()
 		if not utype:
 			print("The User Id and Password combination did not return any results.")
 		else:
-			# print(utype[0][0])
-			operation_choice(utype[0][0])
+			valid = True
 
-		prompt = input("Press enter to continue, or q to exit this program: ")
+	prompt = ''
+	while prompt != 'q':
+		operation_choice(utype[0][0])
+
+		prompt = input("Press enter to continue, or q to close the program: ")
 		
 	
 
@@ -47,7 +47,7 @@ def operation_choice(user_type):
 	# The operations are different for each user type
 
 	options_list = []
-	print("Select an operation: ")
+	
 	print()
 
 	if user_type == "a":
@@ -73,35 +73,73 @@ def operation_choice(user_type):
 		for i in range(len(options_list)):
 			print(str(i+1)+". "+ options_list[i])
 		print()
-		choice = input("Enter the number corresponding to the desired operation: ")
-		if choice == "1":
-			issue_ticket()
-		elif choice == "2":
-			find_car_owner()
+		complete = False
+		while not complete:
+			print("Enter the number corresponding to the desired operation, ")
+			choice = input("Or enter q to exit this prompt: ")
+			if choice == "1":
+				complete = issue_ticket()
+			elif choice == "2":
+				complete = find_car_owner()
+			elif choice == "q":
+				break
 
 
 	
 
 def issue_ticket():
-	global cursor, connection
-	regno = input("Please provide a registration number to view information: ")
+	global cursor, connection, uniqueTNOs
+	# Make sure to change input value for regno to int!!
+	regno = input("\nPlease provide a registration number to view information: ")
 	cursor.execute('''select fname, lname, make, model, year, color 
 					  from registrations join vehicles using (vin)
 					  where regno=?''', (regno,))
 	rows = cursor.fetchall()
 	if not rows:
 		print("The provided registration number does not exist")
-	else:
-		info_for_regno = rows[0]
-		print("Information for registration number:", regno)
+		return False
+
+	info_for_regno = rows[0]
+	success = False
+	while success == False:
+		print("\nInformation for registration number:", regno)
 		print()
 		for column in info_for_regno:
 			print(column, end="  ")
+		print("\n")
+		print("Issue a ticket to registration number: "+ str(regno) + "?")
+		prompt = input("(Press Enter to continue, q to exit): ")
+		if prompt == "q":
+			break
+		print("Please provide the following information:\n")
+		vdate = input("Violation date: ")
+		violation = input("Violation (text): ")
+		fine = input("Fine amount: ")
 		print()
+		
+		# generate a random tno, which might not be unique. An exception is used to handle this.
+		tno = random.sample(range(100000, 999999), 1)[0]
+
+		try:
+			cursor.execute('''insert into tickets values
+							   (?, ?, ?, ?, ?);''', (tno, regno, fine, violation, vdate))
+			connection.commit()
+			print("A ticket (tno: "+str(tno)+ ") has been issued to registration number " + regno + ".")
+			success = True
+		except sqlite3.IntegrityError as nonUniqueTNO:
+			print("An error occured while generating a TNO. Please try again.")
+
+	
+	
+
+	
+	
+
+	return True
 	
 
 def find_car_owner():
-	pass
+	return True
 def register_a_birth():
 	pass
 
