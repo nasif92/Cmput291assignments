59c8209ce7f882d5efd66e296fb9237aa37eeb4d
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-10-25 00:07:13 -0600

    added dynamic query for find_car_owner

--- miniProj1/system.py
+++ miniProj1/system.py
@@ -3,7 +3,6 @@ import random
 
 connection = None
 cursor = None
-uniqueTNOs = []
 
 def main():
 	global connection, cursor
@@ -19,6 +18,7 @@ def main():
 	while valid == False:
 		print("\nEnter your login details:")
 		uid = input("User ID: ")
+		uid = uid.lower()
 		pwd = input("Password: ")
 		print()
 		cursor.execute("select utype from users where uid=? and pwd=?", (uid, pwd))
@@ -88,12 +88,12 @@ def operation_choice(user_type):
 	
 
 def issue_ticket():
-	global cursor, connection, uniqueTNOs
-	# Make sure to change input value for regno to int!!
+	global cursor, connection
+	######## Make sure to change input value for regno to int!! #######
 	regno = input("\nPlease provide a registration number to view information: ")
 	cursor.execute('''select fname, lname, make, model, year, color 
 					  from registrations join vehicles using (vin)
-					  where regno=?''', (regno,))
+					  where regno=?''', [regno])
 	rows = cursor.fetchall()
 	if not rows:
 		print("The provided registration number does not exist")
@@ -115,7 +115,7 @@ def issue_ticket():
 		print("Please provide the following information:\n")
 		vdate = input("Violation date: ")
 
-		# The violation date should be set to today's date if it is not provided.
+		####### The violation date should be set to today's date if it is not provided! #######
 		violation = input("Violation (text): ")
 		
 		fine = input("Fine amount: ")
@@ -133,17 +133,79 @@ def issue_ticket():
 		except sqlite3.IntegrityError as nonUniqueTNO:
 			print("An error occured while generating a TNO. Please try again.")
 
-	
-	
-
-	
-	
-
 	return True
 	
 
 def find_car_owner():
+	global cursor, connection
+	# user provides one or more of make, model, year, color, and plate.
+
+	# If there are more than 4 matches, you will show only the make, model, year, color, and the plate of the matching cars and let the user select one
+	
+	## i.e. more GENERAL
+	# make, model, year, color, plate
+	
+	# When there are less than 4 matches or when a car is selected from a list 
+	# shown earlier, for each match, the make, model, year, color, and the plate of 
+	# the matching car will be shown as well as the latest registration date, the expiry date, 
+	# and the name of the person listed in the latest registration record.
+	
+	## i.e. more PRECISE
+	# make, model, year, color, plate, regdate, expiry, fname, lname
+
+	finalQuery = '''select make, model, year, color, plate, regdate, expiry, fname, lname
+	   				from registrations r join vehicles v using(vin) 
+	   				where ''' # make='Porsche' and model='Boxter' and year='2017' and color='pink' and plate='CASSIE';	<--- Ideally
+	make = input("make: ")
+	model = input("model: ")
+	year = input("year: ")
+	color = input("color: ")
+	plate = input("plate: ")
+	
+	parameters = []
+	if make != "":
+		finalQuery += "make=? and "#, (Porsche)
+		# " and "
+		parameters.append(make)
+	if model != "":
+		finalQuery += "model=? and "
+		# " and "
+		parameters.append(model)
+	if year != "":
+		finalQuery += "year=? and "
+		# " and "
+		parameters.append(year)
+	if color != "":
+		finalQuery += "color=? and "
+		# " and "
+		parameters.append(color)
+	if plate != "":
+		finalQuery += "plate=? and "
+		# ";"
+		parameters.append(plate)
+
+	finalQuery = finalQuery[:-5]
+	finalQuery += ";"
+	print(finalQuery)
+
+	# cursor.execute(finalQuery, parameters)
+	# rows = cursor.fetchall()
+	# count = 0
+	# for match in rows:
+	# 	count += 1
+
+	# if count > 4:
+	# 	# print(make, model, year, color, plate)
+	# 	pass
+	# else:
+	# 	# print(make, model, year, color, plate, regdate, expiry, fname, lname)
+	# 	pass
+
+
 	return True
+
+
+
 def register_a_birth():
 	pass
 
