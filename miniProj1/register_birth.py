9ea913d86d7bc6c73db425314668d8a27c30e48a
Nasif <nhossain@ualberta.ca>
2019-10-31 16:08:21 -0600

    Added q1, lemme know how 1,2 and 3 goes

--- miniProj1/register_birth.py
+++ miniProj1/register_birth.py
@@ -16,7 +16,7 @@ def connect(path):
     return
 
 
-def getBirthInfo():
+def getBirthInfo(regplace):
 	global connection, cursor
 	given = False
 	# check if child's information is given or not
@@ -42,7 +42,7 @@ def getBirthInfo():
 		print("Father's information is present in database")
 	else:
 		print("Father's information not in database. \nPlease provide the required information for creating a new person")
-		father = createPerson(f_fname,f_lname,False)
+		father = createPerson(f_fname.capitalize(),f_lname.capitalize(),False)
 		cursor.execute('''INSERT INTO persons VALUES (?,?,?,?,?,?)''',father) # have to push parents to database
 		print("Done! You created the father's information")
 	given = False
@@ -55,14 +55,15 @@ def getBirthInfo():
 			print("You have to input mother's both first name and last name")
 	if (getPerson(m_fname, m_lname)): # if mother is not there get her information
 		print("Mother's information is present in database")
-		cursor.execute('''SELECT address,phone from persons WHERE fname = ? AND lname = ?''',(m_fname,m_lname))
+		cursor.execute('''SELECT address,phone from persons WHERE fname = ? COLLATE NOCASE AND lname = ?
+		COLLATE NOCASE''',(m_fname.capitalize(),m_lname.capitalize()))
 		info = cursor.fetchall()
 		address = info[0][0]  # mother's address
 		phone = info[0][1] # mother's phone
 		
 	else:
 		print("Mother is not there in your database. \nPlease provide the required information for creating a new person")
-		mother = createPerson(m_fname, m_lname,False)
+		mother = createPerson(m_fname.capitalize(), m_lname.capitalize(),False)
 		address = mother[4]  # mother's address
 		phone = mother[5] # mother's phone
 		cursor.execute('''INSERT INTO persons VALUES (?,?,?,?,?,?)''',mother) # have to push parents to database
@@ -72,16 +73,16 @@ def getBirthInfo():
 	regdate = getDate()
 	connection.commit()
 	regno = getUnique("births", "regno") # getting a unique registration number
-	regplace = 'Test' # this is going to be the user's registration place
+	#regplace = 'Test' # this is going to be the user's registration place
 
-	return regno, fname, lname, regdate, regplace, gender, f_fname, f_lname, m_fname, m_lname
+	return regno, fname, lname, regdate, regplace, gender, f_fname.capitalize(), f_lname.capitalize(), m_fname.capitalize(), m_lname.capitalize()
 
 #births(regno, fname, lname, regdate, regplace, gender, f_fname, f_lname, m_fname, m_lname)
 #persons(fname, lname, bdate, bplace, address, phone) 
 #regno is unique
 #regplace is city of user
 #regdate is today's date
-def register_a_birth():
+def register_a_birth(regplace):
 	global cursor, connection
 
 	prompt = input ("Register for birth? (y/n)")
@@ -91,7 +92,7 @@ def register_a_birth():
 			print("Putting into births database ...")
 			# register that new person into registry
 			cursor.execute('''INSERT or replace INTO births
-			VALUES (?,?,?,?,?,?,?,?,?,?);''',getBirthInfo())
+			VALUES (?,?,?,?,?,?,?,?,?,?);''',getBirthInfo(regplace))
 			# create a person				
 			connection.commit()
 			print("Well done! You have registered a new birth")
@@ -103,106 +104,14 @@ def register_a_birth():
 # register a marriage
 # marriages(regno, regdate, regplace, p1_fname, p1_lname, p2_fname, p2_lname)
 
-def register_marriage():
-	global cursor, connection
-	prompt = input ("Register marriage? (y/n) ")
-	if prompt is 'y' or prompt is 'Y':
-		given = False
-		while not given:
-			p1_fname = input("First Name of partner 1: ")
-			p1_lname = input("Last Name of partner 1: ")
-			if p1_fname != "" and p1_lname != "":
-				given = True
-			else:
-				print("You have to provide partner 1's first name and last name")
-		if (getPerson(p1_fname, p1_lname)): # if partner1 is not a person, get the information about partner
-			print("Partner1's information is present in database")
-		else:
-			print("Person named %s %s is not there in your database. \nPlease provide the required information for creating a new person" %(p1_fname,p1_lname))
-			partner1 = createPerson(p1_fname,p1_lname,False)
-			cursor.execute('''INSERT INTO persons VALUES (?,?,?,?,?,?)''',partner1) # have to push parents to database
-			connection.commit()
-			print("Done! You created the partner 1's (%s %s)'s information" %(p1_fname,p1_lname))
-		given = False
-		while not given:
-			p2_fname = input("First Name of partner 2: ")
-			p2_lname = input("Last Name of partner 2: ")
-			if p2_fname != "" and p2_lname != "":
-				given = True
-			else:
-				print("You have to provide partner 2's first name and last name")
-		if (getPerson(p2_fname, p2_lname)): # if partner2 is not a person, get the information about partner
-			print("Partner2's information is present in database")
-		else:
-			print("Person named %s %s is not there in your database. \nPlease provide the required information for creating a new person" %(p2_fname,p2_lname))
-			partner2 = createPerson(p2_fname,p2_lname,False)
-			cursor.execute('''INSERT or REPLACE INTO persons VALUES (?,?,?,?,?,?)''',partner2) # have to push parents to database
-			connection.commit()
-			print("Done! You created the partner 1's (%s %s)'s information" %(p2_fname,p2_lname))
-		regno = getUnique("marriages","regno")
-		regdate = getDate()
-		regplace = "Test"
-		cursor.execute('''INSERT or REPLACE INTO marriages VALUES(?,?,?,?,?,?,?)''',(regno, regdate, regplace, p1_fname, p1_lname, p2_fname, p2_lname))
-		connection.commit()
-	else:
-		print("Alright!")
 
-
-# registrations(regno, regdate, expiry, plate, vin, fname, lname)
-def renew_registration(regno):
-	global cursor, connection
-	prompt = input ("Renew registration for regno: %s? (y/n) " %regno)
-	if prompt is 'y' or prompt is 'Y':
-		try:
-			cursor.execute('''SELECT expiry from registrations WHERE regno = ?''', (regno,))
-			expiry = cursor.fetchone()
-			currentDate = getDate()
-			start = datetime.datetime.strptime(expiry[0],"%Y-%m-%d")
-			end = datetime.datetime.strptime(currentDate,"%Y-%m-%d")
-			diff = (start - end).days
-			# today's date plus one year if negative
-			# else plus one year that day given
-			if diff <= 0:
-				newDate = str(int(currentDate[0:4]) + 1) + datetime.datetime.now().strftime("-%m-%d")
-			else:
-				newDate = str(int(expiry[0][0:4]) + 1) + expiry[0][4:]
-			cursor.execute('''UPDATE registrations set expiry = ? WHERE regno = ?''',(newDate,regno))
-			connection.commit()
-		except:
-			print("Given registration number %s is not present in database" %regno)
-	else:
-		print("Alright!")
 def main():
 	global connection, cursor
 	connect("./test.db")
 	# first screen
-	print("Welcome to the program.")
-	register_a_birth() # this is functional already
-	register_marriage()
-	renew_registration(999999999) # this is functional too
-
-	connection.commit()
-	print("Every birth : ")
-	print(40*"=")
-	cursor.execute('''SELECT * from births''')
-	all = cursor.fetchall()
+	#print("Welcome to the program.")
+	#register_a_birth("Seattle")
+	#cursor.execute('''SELECT fname, lname from births''')
+	#all = cursor.fetchall()
 	#print(all)
-	print("Every marriage : ")
-	print(40*"=")
-	cursor.execute('''SELECT * from marriages''')
-	all = cursor.fetchall()
-	print(all)
-	print("Every registrations : ")
-	print(40*"=")
-	cursor.execute('''SELECT * from registrations''')
-	all = cursor.fetchall()
-	#print(all)
-	print("Every person : ")
-	print(40*"=")
-	cursor.execute('''SELECT * from persons''')
-	all = cursor.fetchall()
-	#print(all)
-
-	#renew_registration(999999999)
-	#print(regno)
 main() 
